0aba3c1d8e982a729c8ff78f0583834718eaae21
abdelG-coder <55768813+abdelG-coder@users.noreply.github.com>
2024-08-29 13:54:00 +0200

    fix create group

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "web"  {
 }
 
 resource "aws_security_group_rule" "web_http_in" {
-  type = "ingresss"
+  type = "ingress"
   from_port = 80
   to_port = 80
   protocol = "tcp"
@@ -47,7 +47,7 @@ resource "aws_security_group_rule" "web_http_in" {
 }
 
 resource "aws_security_group_rule" "web_https_in" {
-    type = "ingresss"
+    type = "ingress"
   from_port = 443
   to_port = 443
   protocol = "tcp"
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "web_https_in" {
 }
 
 resource "aws_security_group_rule" "web_http_out" {
-    type = "egresss"
+    type = "egress"
   from_port = 0
   to_port = 0
   protocol = "-1"
